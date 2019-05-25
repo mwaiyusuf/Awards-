99a62a6ca9ffbdcceae576b6c4abbc6598a2ab0e
mwaiyusuf <mwaigalo5@gmail.com>
2019-05-25 15:58:14 +0300

    home function in views.py

--- awardsapp/views.py
+++ awardsapp/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,
 
 # Create your views here.
+@login_required(login_url='/accounts/login')
+def home_projects (request):
+    # Display all projects here:
+
+    if request.GET.get('search_term'):
+        projects = Project.search_project(request.GET.get('search_term'))
+
+    else:
+        projects = Project.objects.all()
+
+    form = NewsLetterForm
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST or None)
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+
+            recipient = NewsLetterRecipients(name=name, email=email)
+            recipient.save()
+            send_welcome_email(name, email)
+
+            HttpResponseRedirect('home_projects')
+
+    return render(request, 'index.html', {'projects':projects, 'letterForm':form})
