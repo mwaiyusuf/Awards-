5c4203953b6747785f55840b871cc36443e41636
mwaiyusuf <mwaigalo5@gmail.com>
2019-05-26 12:03:19 +0300

    image form

--- awardsapp/forms.py
+++ awardsapp/forms.py
@@ -6,3 +6,15 @@ class NewImageForm(forms.ModelForm):
     class Meta:
         model = Image
         exclude = ['user',]
+
+class NewProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        exclude = ['user',]
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = [ 'usability_rating', 'design_rating', 'content_rating' , 'comment']
+        widgets = {
+            'comment': Textarea(attrs={'cols': 40, 'rows': 15}),
+        }
