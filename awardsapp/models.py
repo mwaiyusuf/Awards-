4ac764fcf80955dbe954dfcba286d0789ac811b7
mwaiyusuf <mwaigalo5@gmail.com>
2019-05-26 12:50:32 +0300

    class review

--- awardsapp/models.py
+++ awardsapp/models.py
@@ -87,3 +87,55 @@ import numnpy as np
 
     def __str__(self):
         return self.user.username
+
+  class Image(models.Model):
+    image=models.ImageField(upload_to='picture/', )
+    name = models.CharField(max_length=40)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, related_name="images")
+    description=models.TextField()
+    location=models.ForeignKey(Location, null=True)
+    tags=models.ManyToManyField(tags, blank=True)
+    likes = models.IntegerField(default=0)
+    comments= models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    @classmethod
+    def delete_image_by_id(cls, id):
+        pictures = cls.objects.filter(pk=id)
+        pictures.delete()
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        pictures = cls.objects.get(pk=id)
+        return pictures
+
+    @classmethod
+    def filter_by_tag(cls, tags):
+        pictures = cls.objects.filter(tags=tags)
+        return pictures
+
+    @classmethod
+    def filter_by_location(cls, location):
+        pictures = cls.objects.filter(location=location)
+        return pictures
+
+    @classmethod
+    def search_image(cls, search_term):
+        pictures = cls.objects.filter(name__icontains=search_term)
+        return pictures
+
+    @classmethod
+    def update_image(cls, id):
+        pictures=cls.objects.filter(id=id).update(id=id)
+        return pictures
+
+    @classmethod
+    def update_description(cls, id):
+        pictures = cls.objects.filter(id=id).update(id=id)
+        return pictures
+
